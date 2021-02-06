0216f1408664025986eee1abdcc1cb9ff3c3feaa
utsab-bong <utsav.festiv@gmail.com>
2021-02-06 12:55:27 +0530

    Revert "B1 (#3)"
    This reverts commit d12174853d9efea0f92b562ef1bf70b8c2658f24.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("bal");
-        System.out.println("to be reverted1");
         System.out.println("chal");
-        System.out.println("to be reverted2");
-        // write your code here
+	// write your code here
     }
 }
