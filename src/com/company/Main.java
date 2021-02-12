4e282f8a8171c017982f23880b180620e06f2e9f
Utsab Banerjee <utsav.festiv@gmail.com>
2021-02-12 09:05:20 +0530

    commit chk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("bal");
-        System.out.println("chal");
+        System.out.println("bal!");
+        System.out.println("chal!");
 	// write your code here
     }
 }
